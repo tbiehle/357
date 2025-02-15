e1063ccb7952c18e67e4a0aaa35f25e85b2995af
Travis B <travis.biehle@gmail.com>
2025-02-14 18:05:30 -0800

    basic image mixing works

--- asgn2/asgn2.cpp
+++ asgn2/asgn2.cpp
@@ -45,7 +45,7 @@ int main()
     bih bih1, bih2;
 
     FILE *img1 = fopen("flowers.bmp", "rb");
-    FILE *img2 = fopen("lion.bmp", "rb");
+    FILE *img2 = fopen("wolf.bmp", "rb");
 
     // read header info of img1
     fread(&bfh1.bfType, 2, 1, img1);
@@ -85,7 +85,7 @@ int main()
 
     bih infoh, infohsmall; // infoh: bigger file info header 
     bfh fileh, filehsmall; // fileh: bigger file file header
-    FILE *bfile, *sfile; // pointer to bigger and smaller files
+    FILE *bfile, *sfile; // pointer to bigger and smaller images
 
     if (bih1.biWidth > bih2.biWidth) // img1 is bigger than img2, set proper info for outfile
         {
@@ -109,9 +109,10 @@ int main()
     BYTE *bigdata = (BYTE *)malloc(fileh.bfSize); // allocate space to store data of big image
     fread(bigdata, fileh.bfSize, 1, bfile);
     BYTE *smalldata = (BYTE *)malloc(filehsmall.bfSize);
-    fread(smalldata, fileh.bfSize, 1, sfile);
+    fread(smalldata, filehsmall.bfSize, 1, sfile);
     
-    FILE *outfile = fopen("outfile.bmp", "wb");
+    FILE *outfile = fopen("outimg.bmp", "wb"); // create output file 
+    // write info from bigger image into outfile header
     fwrite(&fileh.bfType, 2, 1, outfile);
     fwrite(&fileh.bfSize, 4, 1, outfile);
     fwrite(&fileh.bfReserved1, 2, 1, outfile);
@@ -128,7 +129,59 @@ int main()
     fwrite(&infoh.biYPelsPerMeter, 4, 1, outfile);
     fwrite(&infoh.biClrUsed, 4, 1, outfile);
     fwrite(&infoh.biClrImportant, 4, 1, outfile);
-    fwrite(bigdata, fileh.bfSize, 1, outfile);
+
+    int wib = 3 * infoh.biWidth; // width in bytes: biWidth is width in pixels
+    int padding = (4 - wib % 4) % 4;
+    int rwib = wib + padding; // account for padding
+
+    wib = 3 * infohsmall.biWidth;
+    padding = (4 - wib % 4) % 4;
+    int rwib_sm = wib + padding;
+
+    for (int y = 0; y < infoh.biHeight; y++)
+        {
+        for (int x = 0; x < infoh.biWidth; x++)
+            {
+                BYTE *pix = &bigdata[3 * x + y * rwib];
+
+                BYTE br, ir;
+                br = ir = pix[2];
+                BYTE bg, ig;
+                bg = ig = pix[1];
+                BYTE bb, ib;
+                bb = ib = pix[0];
+
+
+                if (x < infohsmall.biWidth && y < infohsmall.biHeight) // within bounds of small image
+                    {
+                    BYTE *pixsm = &smalldata[3 * x + y * rwib_sm];
+
+                    BYTE sr = pixsm[2];
+                    BYTE sg = pixsm[1];
+                    BYTE sb = pixsm[0];
+
+                    float ratio = 0.3;
+                    ir = br * (1 - ratio) + sr * ratio;
+                    ig = bg * (1 - ratio) + sg * ratio;
+                    ib = bb * (1 - ratio) + sb * ratio;
+                    }
+
+                // do some stuff
+                
+
+                pix[2] = ir;
+                pix[1] = ig;
+                pix[0] = ib;
+
+                fwrite(pix, 3, 1, outfile);
+            }
+        }
+
+    free(bigdata);
+    free(smalldata);
+    fclose(img1);
+    fclose(img2);
+    fclose(outfile);
 
     return 0;
 }
