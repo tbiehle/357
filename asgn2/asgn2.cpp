027a9495085eb91a43dfa0c8f580b7405d3ee511
Travis B <travis.biehle@gmail.com>
2025-02-17 11:39:23 -0800

    parallel works, next up: command line args

--- asgn2/asgn2.cpp
+++ asgn2/asgn2.cpp
@@ -10,6 +10,7 @@
 #include <dirent.h>
 #include <fcntl.h>
 #include <ctime>
+#include <string.h>
 
 using namespace std;
 
@@ -77,7 +78,7 @@ void get_neighbor_pix(BYTE *neighbors[4], BYTE *data, int x, int y, int w, int h
 }
 
 
-int main()
+int main(int argc, char *argv[])
 {
     bfh bfh1, bfh2;
     bih bih1, bih2;
@@ -85,7 +86,7 @@ int main()
     FILE *img1 = fopen("mario.bmp", "rb");
     FILE *img2 = fopen("tunnel.bmp", "rb");
     float ratio = 0.5;
-    int num_processes = 2;
+    int num_processes = 4;
 
     // read header info of img1
     fread(&bfh1.bfType, 2, 1, img1);
@@ -146,11 +147,75 @@ int main()
         filehsmall = bfh1;
         }
     
-    BYTE *bigdata = (BYTE *)malloc(fileh.bfSize); // allocate space to store data of big image
+    //BYTE *bigdata = (BYTE *)malloc(fileh.bfSize); // allocate space to store data of big image
+    BYTE *bigdata = (BYTE *)mmap(NULL, fileh.bfSize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     fread(bigdata, fileh.bfSize, 1, bfile);
-    BYTE *smalldata = (BYTE *)malloc(filehsmall.bfSize);
+    BYTE *smalldata = (BYTE *)mmap(NULL, filehsmall.bfSize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     fread(smalldata, filehsmall.bfSize, 1, sfile);
-    
+    BYTE *outdata = (BYTE *)mmap(NULL, fileh.bfSize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    int wib = 3 * infohsmall.biWidth; // width in bytes: biWidth is width in pixels
+    int padding = (4 - wib % 4) % 4;
+    int rwib_sm = wib + padding; // account for padding
+
+    wib = 3 * infoh.biWidth;
+    padding = (4 - wib % 4) % 4;
+    int rwib = wib + padding;
+
+    float w_size_ratio = (float)infohsmall.biWidth / (float)infoh.biWidth;
+    float h_size_ratio = (float)infohsmall.biHeight / (float)infoh.biHeight;
+
+    clock_t s_time = clock();
+
+    pid_t child_pid, wpid;
+    int status = 0;
+
+    int rows_per_process = infoh.biHeight / num_processes;
+    for (int j = 0; j < num_processes; j++)
+        {
+        child_pid = fork();
+        if (child_pid == 0)
+            {
+            int start_row = j * rows_per_process;
+            int end_row = (j == num_processes - 1) ? infoh.biHeight : (j + 1) * rows_per_process;
+            
+            for (int y = start_row; y < end_row; y++)
+                {
+                for (int x = 0; x < infoh.biWidth; x++)
+                    {
+                    float fxs = (float) x * w_size_ratio; // x cords of big img in terms of small
+                    int ixs = (int) fxs;
+                    float fys = (float) y * h_size_ratio; // y of small i.t.o. big
+                    int iys = (int) fys;
+
+                    float dx = fxs - ixs; // delta value x
+                    float dy = fys - iys; // delta value y
+                    
+                    int pix_idx = 3 * x + y * rwib;
+                    BYTE *pix = &bigdata[pix_idx];
+                    
+                    BYTE i_big[3];
+                    BYTE big[3] = {pix[0], pix[1], pix[2]};
+
+                    BYTE *surr_pix[4];
+                    get_neighbor_pix(surr_pix, smalldata, ixs, iys, infohsmall.biWidth, infohsmall.biHeight, rwib_sm); // four surrounding pixels
+
+                    BYTE i_small[3];
+                    for (int i = 0; i < 3; i++)
+                        {
+                        i_small[i] = interp_color(surr_pix, i, dx, dy);
+                        pix[i] = big[i] * (1 - ratio) + ratio * i_small[i];
+                        outdata[pix_idx + i] = pix[i];
+                        }
+                    }
+                }
+
+            exit(0);
+            }
+        }
+
+    while ((wpid = wait(&status)) > 0);
+
     FILE *outfile = fopen("outimg.bmp", "wb"); // create output file 
     // write info from bigger image into outfile header
     fwrite(&fileh.bfType, 2, 1, outfile);
@@ -169,61 +234,13 @@ int main()
     fwrite(&infoh.biYPelsPerMeter, 4, 1, outfile);
     fwrite(&infoh.biClrUsed, 4, 1, outfile);
     fwrite(&infoh.biClrImportant, 4, 1, outfile);
+    fwrite(outdata, fileh.bfSize, 1, outfile);
 
-    int wib = 3 * infohsmall.biWidth; // width in bytes: biWidth is width in pixels
-    int padding = (4 - wib % 4) % 4;
-    int rwib_sm = wib + padding; // account for padding
-
-    wib = 3 * infoh.biWidth;
-    padding = (4 - wib % 4) % 4;
-    int rwib = wib + padding;
-
-    float w_size_ratio = (float)infohsmall.biWidth / (float)infoh.biWidth;
-    float h_size_ratio = (float)infohsmall.biHeight / (float)infoh.biHeight;
-
-    clock_t s_time = clock();
-
-    for (int y = 0; y < infoh.biHeight; y++)
-        {
-        for (int x = 0; x < infoh.biWidth; x++)
-            {
-                float fxs = (float) x * w_size_ratio; // x cords of big img in terms of small
-                int ixs = (int) fxs;
-                float fys = (float) y * h_size_ratio; // y of small i.t.o. big
-                int iys = (int) fys;
-
-                float dx = fxs - ixs; // delta value x
-                float dy = fys - iys; // delta value y
-                
-                BYTE *pix = &bigdata[3 * x + y * rwib];
-                
-                BYTE i_big[3];
-                BYTE big[3] = {pix[0], pix[1], pix[2]};
-
-                BYTE *surr_pix[4];
-                get_neighbor_pix(surr_pix, smalldata, ixs, iys, infohsmall.biWidth, infohsmall.biHeight, rwib_sm); // four surrounding pixels
-
-                BYTE i_small[3];
-                for (int i = 0; i < 3; i++)
-                    {
-                    i_small[i] = interp_color(surr_pix, i, dx, dy);
-                    pix[i] = big[i] * (1 - ratio) + ratio * i_small[i];
-                    }
-
-                fwrite(pix, 3, 1, outfile);
-            }
-            for (int i = 0; i < padding; i++)
-            {
-                fputc(0, outfile);
-            }
-        }
-
-    wait(0);
     clock_t f_time = clock();
     cout << "total time: " << (double) (f_time - s_time) << endl;
 
-    free(bigdata);
-    free(smalldata);
+    munmap(bigdata, sizeof(bigdata));
+    munmap(smalldata, sizeof(smalldata));
     fclose(img1);
     fclose(img2);
     fclose(outfile);
