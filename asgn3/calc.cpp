21ffb89dd5479515097b6b7dda98f4e8d6800d06
Travis B <travis.biehle@gmail.com>
2025-02-24 23:12:40 -0800

    cleaned up and ready for submission

--- asgn3/calc.cpp
+++ asgn3/calc.cpp
@@ -9,6 +9,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#define VALUE_RANGE 10;
+#define MATRIX_WIDTH 100;
+
 using namespace std;
 
 void given_sync(int par_id, int par_count, int*ready) {
@@ -45,8 +48,8 @@ void matrix_mult(int *matA, int *matB, int *mat_out, int start_row, int end_row,
     }
 }
 
-int diagonal_sum(int *mat, int mat_w) {
-    int sum = 0;
+long long diagonal_sum(int *mat, int mat_w) {
+    long long sum = 0;
     for (int i = 0; i < mat_w; i++) {
         sum += mat[i + i * mat_w];
     }
@@ -55,25 +58,38 @@ int diagonal_sum(int *mat, int mat_w) {
 }
 
 void generate_matrix(int *mat, int mat_size) {
+    
+
     for (int i = 0; i < mat_size; i++) {
-        mat[i] = 1;
-        //mat[i] = rand() % 3; // generate random number between 0 and 2
+        mat[i] = rand() % VALUE_RANGE; // generate random number between 0 and 2
     }
 }
 
 int main(int argc, char* argv[]) {
     int par_id = atoi(argv[1]);
     int par_count = atoi(argv[2]);
+    // par_id: what process this is
+    // par_count: how many processes there are 
+
     int *matA, *matB, *matM; // matrix pointers
     int matA_fd, matB_fd, matM_fd; // matrix file descriptors
     int *ready; // synch pointer
+
+    int *row_tracker; 
+    // remainder pointer for keeping track of extra work
+    // index 0 is extra rows needed to be processed
+    // index 1 is running total of how many rows have been processed, i.e. assigned to each process
+    // so if the remainder is 4, the first four processes get an extra row
+
+    int remainder_fd;
     int ready_fd;
-    int mat_w = 20;
+    int mat_w = MATRIX_WIDTH;
     int mat_size = mat_w * mat_w;
 
-    // UNCOMMENT THIS TO MAKE TRUE RANDOM VALUES
-    // srand(time(0)); // initialize random seed based on cpu time
-
+    int par_rows = mat_w / par_count; // ex) if matrix is 2x2 and there are 2 processes, one row per process
+    int processed_rows = par_rows * par_count;
+    int remaining_rows = mat_w - processed_rows;
+    
     if (par_id == 0) {
         // first process, create shared memory
         // matrix A
@@ -81,7 +97,8 @@ int main(int argc, char* argv[]) {
         matB_fd = shm_open("matB_mem", O_RDWR | O_CREAT, 0777);
         matM_fd = shm_open("matM_mem", O_RDWR | O_CREAT, 0777);
         ready_fd = shm_open("ready_mem", O_RDWR | O_CREAT, 0777);
-        if (matA_fd == -1 || matB_fd == -1 || matM_fd == -1 || ready_fd == -1) {
+        remainder_fd = shm_open("remainder_mem", O_RDWR | O_CREAT, 0777);
+        if (matA_fd == -1 || matB_fd == -1 || matM_fd == -1 || ready_fd == -1 || remainder_fd == -1) {
             perror("shm_open");
             exit(1);
         }
@@ -89,13 +106,21 @@ int main(int argc, char* argv[]) {
         ftruncate(matB_fd, mat_size * sizeof(int));
         ftruncate(matM_fd, mat_size * sizeof(int));
         ftruncate(ready_fd, (par_count + 1) * sizeof(int));
+        ftruncate(remainder_fd, 2 * sizeof(int));
         matA = (int *)mmap(NULL, mat_size * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, matA_fd, 0);
         matB = (int *)mmap(NULL, mat_size * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, matB_fd, 0);
         matM = (int *)mmap(NULL, mat_size * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, matM_fd, 0);
         ready = (int *)mmap(NULL, (par_count + 1) * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, ready_fd, 0);
+        row_tracker = (int *)mmap(NULL, 2 * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, remainder_fd, 0);
 
         for (int i = 0; i < par_count; i++) {ready[i] = 0;}
         ready[par_count] = 0;
+
+        row_tracker[0] = remaining_rows;
+        row_tracker[1] = 0;
+
+        // COMMENT THIS OUT TO MAKE NON-RANDOM VALUES
+        srand(time(0)); // initialize random seed based on cpu time
 
         generate_matrix(matA, mat_size);
         generate_matrix(matB, mat_size);
@@ -106,26 +131,37 @@ int main(int argc, char* argv[]) {
         while (!(matB_fd = shm_open("matB_mem", O_RDWR, 0777)));
         while (!(matM_fd = shm_open("matM_mem", O_RDWR, 0777)));
         while (!(ready_fd = shm_open("ready_mem", O_RDWR, 0777)));
+        while (!(remainder_fd = shm_open("remainder_mem", O_RDWR, 0777)));
         // link matrix pointers to shared memomry
         matA = (int *)mmap(NULL, mat_size * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, matA_fd, 0);
         matB = (int *)mmap(NULL, mat_size * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, matB_fd, 0);
         matM = (int *)mmap(NULL, mat_size * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, matM_fd, 0);
         ready = (int *)mmap(NULL, (par_count + 1) * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, ready_fd, 0);
-        if (matA == MAP_FAILED || matB == MAP_FAILED || matM == MAP_FAILED || ready == MAP_FAILED) {
+        row_tracker = (int *)mmap(NULL, 2 * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, remainder_fd, 0);
+        if (matA == MAP_FAILED || matB == MAP_FAILED || matM == MAP_FAILED || ready == MAP_FAILED || row_tracker == MAP_FAILED) {
             perror("mmap");
             exit(1);
         }
-        sleep(1);
+        //sleep(1);
     }
 
-    // par_id: what process this is
-    // par_count: how many processes there are 
+    int start_row = row_tracker[1];
 
-    int par_rows = mat_w / par_count; // ex) if matrix is 2x2 and there are 2 processes, one row per process
-    int start_row = par_id * par_rows;
+    if (row_tracker[0] > 0) {
+        par_rows += 1;
+        row_tracker[0] -= 1;
+    }
+    row_tracker[1] += par_rows;
+
     int end_row = start_row + par_rows;
 
+    timespec s_time, f_time;
+    
     given_sync(par_id, par_count, ready);
+    if (par_id == 0) {
+        clock_gettime(CLOCK_MONOTONIC, &s_time);
+    }
+
     // M = A * B
     matrix_mult(matA, matB, matM, start_row, end_row, mat_w);
 
@@ -139,20 +175,28 @@ int main(int argc, char* argv[]) {
 
     given_sync(par_id, par_count, ready);
     // diagonal_sum(M)
-    int diag_sum = diagonal_sum(matM, mat_w);
+    long long diag_sum = diagonal_sum(matM, mat_w);
 
-    // print diagonal sum of M
-    if (par_id == 0) cout << diag_sum << endl;
+    given_sync(par_id, par_count, ready);
+
+    if (par_id == 0) {
+        clock_gettime(CLOCK_MONOTONIC, &f_time);
+        long elapsed_time_ns = (f_time.tv_sec - s_time.tv_sec) * 1000000000L + (f_time.tv_nsec - s_time.tv_nsec);
+        std::cout << "Diagonal sum: " << diag_sum << std::endl;
+        std::cout << "Total time: " << elapsed_time_ns << " microseconds" << std::endl;
+    }
 
     close(matA_fd);
     close(matB_fd);
     close(matM_fd);
     close(ready_fd);
+    close(remainder_fd);
     if (par_id == 0) {
         shm_unlink("matA_mem");
         shm_unlink("matB_mem");
         shm_unlink("matM_mem");
         shm_unlink("ready_mem");
+        shm_unlink("remainder_mem");
     }
     return 0;
 }
